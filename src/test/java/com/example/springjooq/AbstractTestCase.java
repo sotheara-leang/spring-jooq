26ae1c5e8bbaf07ac26a448faad8cba796000db8
Sotheara Leang <leangsotheara@gmail.com>
2017-09-09 09:19:46 +0700

    Commit

--- src/test/java/com/example/springjooq/AbstractTestCase.java
+++ src/test/java/com/example/springjooq/AbstractTestCase.java
@@ -1,16 +1,11 @@
 package com.example.springjooq;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = "classpath:conf/context-root.xml")
-public class AbstractTestCase {
+public abstract class AbstractTestCase {
 
-	@Test
-	public void doTest() {
-		
-	}
 }
